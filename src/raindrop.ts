eff35343f21975e1111672d52c4bdba831cdb6d0
erauner <raunerevan@gmail.com>
2022-05-11 21:36:42 -0500

    get all highlights

--- src/raindrop.ts
+++ src/raindrop.ts
@@ -1,22 +1,64 @@
 const RAINDROP_API_BASE = "https://api.raindrop.io/rest/v1/";
 
 const getRaindrops = async (collectionID: number = 0, search: string, sort: string, accessToken: string) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization",  `Bearer ${accessToken}`);
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  };
+
   // console.info('getRaindrops');
 	let params: Record<string, any> = {}
-  let url = new URL(`${RAINDROP_API_BASE}raindrops/${collectionID}`);
+  
+  let raindrops_url = new URL(`${RAINDROP_API_BASE}raindrops/${collectionID}`);
+  let individual_raindrop_url = new URL(`${RAINDROP_API_BASE}raindrop/`);
   
 	if(search) params = {search, ...params}
 	if(sort) params = {sort, ...params}
 
-  url.search = new URLSearchParams(params).toString();
+  raindrops_url.search = new URLSearchParams(params).toString();
 
-  return await fetch(url.toString(), {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-		},
-	})
-  .then((res: any) => res.json())
+  function fetchRaindropDetails(id: number, index: number) {
+    return fetch(individual_raindrop_url.toString() + id, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        return [index, data];
+    });
+  }
+
+  function fetchRaindrops() {
+    return fetch(raindrops_url.toString(), requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+  }
+
+  return (async () => {
+    const data = await fetchRaindrops();
+    const promises = data.items.map((raindrop: any, index: number) => fetchRaindropDetails(raindrop._id, index));
+    await Promise.all(promises).then(responses => {
+      responses.map(response => {
+        data[response[0]] = {...data[response[0]], ...response[1]};
+      })
+    });
+  
+  const results = {
+    "result": (data.items.length > 0) ? true : false,
+    "items": Array<any>(),
+    "count": 0,
+    "collectionId": collectionID
+  }
+  
+  for (const [key, value] of Object.entries(data)) {
+    if (value.item){
+      results.items.push(value.item)
+      results.count += 1
+    }
+  }
+  return results
+  })()
   .then((json: any) => {
     return json;
   });
